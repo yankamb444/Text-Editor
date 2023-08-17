f3895b07e97815e29976cd1cf9487c4301c7da0d
Yandra Kamberaj <yandrakamb@gmail.com>
2023-08-17 17:38:31 -0400

    added stalewhilerevalidate to imports and deployed of heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,9 +1,10 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
 
 precacheAndRoute(self.__WB_MANIFEST);
 
